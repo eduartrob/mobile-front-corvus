67f6650bc1948abc19a28228ef8c387c6e27f7d2
davity333 <reyguz421@gmail.com>
2026-06-22 21:53:06 -0600

    Creacion de las vistas completas de autenticacion(registro,recuperar contraseña y codigo de verificacion)

---
+++ lib/src/page/forgot_password_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/src/plugin/organism/forgotPasswordOrg.dart';
+
+class ForgotPasswordPage extends StatelessWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ForgotPasswordOrg();
+  }
+}
---
+++ lib/src/page/register_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/src/plugin/organism/registerOrg.dart';
+
+class RegisterPage extends StatelessWidget {
+  const RegisterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RegisterOrg();
+  }
+}
