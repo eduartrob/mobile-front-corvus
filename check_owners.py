069d1332d6707732b3a9b79015f95287b948e56d
Esduardo <eduartrob@gmail.com>
2026-07-09 13:53:55 -0600

    chore: remove temporary scripts and md files

--- check_owners.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-# We will use the same token from the backend logs! But I don't have it.
-# Actually, I can use the user's Flutter app to print the ownerId of the courses!
--- patch.dart
+++
@@ -1,3 +0,0 @@
-import 'package:google_sign_in/google_sign_in.dart';
-
-Future<void> main() async {}
