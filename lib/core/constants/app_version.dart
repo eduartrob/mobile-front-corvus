bb03883b1317a9e2d484eba9c5dfda68ccaabe93
Esduardo <eduartrob@gmail.com>
2026-07-22 09:02:24 -0600

    chore: bump version to 2.7.0+21

--- lib/core/constants/app_version.dart
+++ lib/core/constants/app_version.dart
@@ -1,7 +1,7 @@
 /// Versión de la aplicación Corvus.
 /// Actualiza este valor manualmente cuando subas la versión en pubspec.yaml.
 class AppVersion {
-  static const String version = '2.5.5';
-  static const int buildNumber = 20;
+  static const String version = '2.7.0';
+  static const int buildNumber = 21;
   static const String full = '$version+$buildNumber';
 }
